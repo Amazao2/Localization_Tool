8390f8c1a9e37b8c0e7f8f0dec23eaa996ae1dd4
Brandon McRae <bmcrae2@uwo.ca>
2014-11-20 01:42:27 -0500

    Add new Languages
    - extends the editor for LocalizationController to allow adding
    languages through the editor

---
+++ Localization_Tool/Assets/Editor/LocalizationControllerEditor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+using UnityEditor;
+using Assets.Scripts.Localization;
+using System.Linq;
+
+[CustomEditor(typeof (LocalizationController) )]
+public class LocalizationControllerEditor : Editor
+{
+    private string languageCode;
+
+	public override void OnInspectorGUI()
+    {
+        LocalizationController myTarget = (LocalizationController)target;
+        
+
+        DrawDefaultInspector();
+
+        EditorGUILayout.HelpBox("Press play to view supported languages." + 
+            "While in play mode you can add new languages below.", 
+            MessageType.Info
+            );
+
+        // allow adding a new language file
+        EditorGUILayout.PrefixLabel("Add a new Language:");
+        EditorGUI.indentLevel++;
+
+        languageCode = EditorGUILayout.TextField("Language Code", languageCode);
+        if (GUILayout.Button("Add"))
+        {
+            addNewLanguageMessageFile(myTarget, new Lang(languageCode));
+        }
+
+    }
+
+    private void addNewLanguageMessageFile(LocalizationController controller, Lang newLang)
+    {
+        // only create the file if one doesn't already exist
+        if( !controller.getSupportedLanguages().Exists( l => { return l == newLang; } ) )
+        {
+            File.Create(controller.absoluteMessagesPath + "/messages." + newLang.code).Dispose();
+        }
+    }
+
+}
